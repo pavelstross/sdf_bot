f81bcc300f095e6ce35aec6f0a287401a1f716e5
pavelstross <pavel.stross@gmail.com>
2014-04-01 00:10:57 +0200

    basic player list functionality

---
+++ sdf_bot.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/ruby
+
+require 'cinch'
+
+require 'nokogiri'
+require 'open-uri'
+
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.nick = "sdf_bot"
+    c.server = "euroserv.fr.quakenet.org"
+    c.channels = ["#sDF"]
+  end
+
+  on :message, "!help" do |m|
+    m.reply "Available options:\n !players - Current players on the server\n"
+    #//m.reply "llo, #{m.user.nick}"
+  end
+  on :message, "!players" do |m|
+    doc = Nokogiri::HTML(open('http://dpmaster.deathmask.net/?game=openarena&server=195.154.82.77:27960'))
+    replytext = "Current players on sDF server: \n"
+    doc.xpath("//div[@id='handle']").drop(1).each do |p|
+      replytext = replytext + "\t#{p.text} \n"
+    end
+    m.reply replytext
+  end
+end
+
+bot.start
